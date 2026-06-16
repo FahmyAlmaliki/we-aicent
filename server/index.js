11529ea067b1f0ce96a812b4ea0bbad566e36c08
FahmyAlmaliki <fahmy87arm@gmail.com>
2026-06-16 22:45:37 +0700

    update port listen

--- server/index.js
+++ server/index.js
@@ -37,6 +37,6 @@ app.use((error, _req, res, _next) => {
   return res.status(500).json({ message: "Terjadi kesalahan pada server." });
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
